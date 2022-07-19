06f3557a4870e66586849feff514fab345f93d83
soni2 <soni2gf@gmail.com>
2022-07-18 20:46:55 -0400

    adding some conditional

--- src/components/CreateTodo.jsx
+++ src/components/CreateTodo.jsx
@@ -3,21 +3,32 @@ import '../Style/CreateTodo.css'
 
 function CreateTodo({addTodo, forwardRef}) {
   const [textValue, setTextValue] = React.useState('')
+  const [incompleteText, setincompleteText] = React.useState(false)
 
   function saveValue (event){
     setTextValue(event.target.value)
   }
 
-  function createNewTodo(event){
-    event.preventDefault();
-    addTodo(textValue)
-    setTextValue('')
+
+  function createNewTodo(event){    
+    if (textValue <= 1){
+      setincompleteText(true);
+      event.preventDefault();
+
+    }else{
+      event.preventDefault();
+      addTodo(textValue)
+      setTextValue('')
+      setincompleteText(false);
+    }
+    
   }
 
   return (
     <section className='CreateTodo'>
       <form className='TodoForm' onSubmit={createNewTodo}>
         <label className='createTodoLabel'>Crea una tarea</label>
+        {incompleteText && <p style={{color:"red", margin:"4px 0 0"}}>No puedes enviar tareas vacias</p>}
         <textarea 
           value={textValue} 
           placeholder='Escribe una Tarea' 
